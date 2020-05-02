2a25029e2b23033f4b812b5f775da9725f2757a2
RobinLoksli <ami0504@mail.ru>
2020-05-03 00:24:04 +1000

    fix

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,13 +5,11 @@ const UserModel = require('./../models/UserModel');
 const User = new UserModel();
 
 exports.actionIndex = async (req, res) => {
-    console.log();
     let users = await User.find('all', {
         join:[
             ['inner', 'role', 'role_id = role.id'],
         ],
     });
 
-    console.log(req.session.userIndentity);
     res.send(users);
 }
