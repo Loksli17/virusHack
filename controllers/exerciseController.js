a2ecabdf84126e4bb198c8a0dc53cfd528455d01
RobinLoksli <ami0504@mail.ru>
2020-05-03 20:00:37 +1000

    link

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -13,5 +13,50 @@ exports.actionView = async (req, res) => {
 }
 
 exports.actionEdit = async (req, res) => {
+    const
+        GET  = req.query,
+        POST = req.body;
+
+    if(GET.id == undefined){
+        res.render('server/error.hbs', {
+            layout :  null,
+            code   : '404',
+            messege: 'Страница не найдена',
+        });
+        return;
+    }
+
+    let
+        id = Number(GET.id);
+
+    if(isNaN(id)){
+        id = 1;
+    }
+
+    let exercise = {};
+
+    exercise.findById(id);
+
+    if(exercise == undefined){
+        res.render('server/error.hbs', {
+            layout :  null,
+            code   : '404',
+            messege: 'Страница не найдена',
+        });
+        return;
+    }
+
+    if(POST.do == undefined){
+        res.render('exercise/edit', {
+            layout  : 'admin',
+            csrf    : res.locals._csrfToken,
+            roles   : roles,
+            action  : 'edit?id=' + id,
+            fields  : exercise.fields,
+            formData: user,
+        });
+        return;
+    }
+
 
 }
