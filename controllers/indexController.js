4d64c622f3c82cadfcfc53cc50a3a2742e96cc66
BlackLirium <mrfilin97@gmail.com>
2020-05-03 19:48:59 +1000

    admin groups

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -37,7 +37,7 @@ exports.actionIndexTeacher = async (req, res) => {
             'user.firstname as teacherFirstName',
             'user.lastname as teacherLastName',
             'user.patronyc as teacherPatronyc',
-            'groups.title as gtitle'
+            'group.title as gtitle'
         ],
         where: [
             ['user.id = ', req.session.userIndentity.id,' AND '],
@@ -45,7 +45,7 @@ exports.actionIndexTeacher = async (req, res) => {
             ['date >= ', firstDate, ''],
         ],
         join: [
-            ['inner', 'groups', 'exercise.group_id = groups.id'],
+            ['inner', 'group', 'exercise.group_id = group.id'],
             ['inner', 'subject', 'subject.id = exercise.subject_id'],
             ['inner', 'user_has_subject', 'user_has_subject.subject_id = subject.id'],
             ['inner', 'user', 'user_has_subject.user_id = user.id'],
@@ -181,11 +181,18 @@ exports.actionIndexAdmin = async(req, res) => {
     let groups = [];
 
     groups = await Group.find('all', {
+        select : [
+            'group.id',
+            'group.title as gtitle',
+            'course.title as ctitle',
+        ],
         join: [
-            ['inner', 'course', 'groups.course_id = course.id']
+            ['inner', 'course', 'group.course_id = course.id']
         ]
     });
     console.log(groups);
 
-    res.send(groups);
+    res.render('index/adminGroups', {
+        groups : groups,
+    });
 }
