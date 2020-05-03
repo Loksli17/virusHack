1096d4ebcabd2f6aff201834de6ba1ef143f8851
RobinLoksli <ami0504@mail.ru>
2020-05-03 20:49:51 +1000

    css

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -35,8 +35,12 @@ exports.actionEdit = async (req, res) => {
 
     let
         exercise = await Exercise.findById(id),
-        files    = await File
-    console.log(exercise);
+        files    = await File.find('all', {
+            where: [
+                ['exercise_id = ', exercise.id, 'AND'],
+                ['user_id = ', req.session.userIndentity.id, ''],
+            ]
+        });
 
     if(exercise == undefined){
         res.render('server/error.hbs', {
@@ -46,6 +50,7 @@ exports.actionEdit = async (req, res) => {
         });
         return;
     }
+    console.log(files);
 
     if(POST.do == undefined){
         res.render('exercise/edit', {
@@ -53,9 +58,8 @@ exports.actionEdit = async (req, res) => {
             action  : 'edit?id=' + id,
             fields  : exercise.fields,
             formData: exercise,
+            files   : files,
         });
         return;
     }
-
-
 }
