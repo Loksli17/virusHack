7836c10e44be91c1113d70a7b1809fb5c27ce09e
BlackLirium <mrfilin97@gmail.com>
2020-05-03 16:48:09 +1000

    teacher index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -14,26 +14,98 @@ exports.actionIndex = async (req, res) => {
 
 
 exports.actionIndexTeacher = async (req, res) => {
-
-}
-
-
-exports.actionIndexStudent = async (req, res) => {
+    console.log(req.session.userIndentity)
     let
-        datesWeek = DateModule.getDatesWeek(),
-        exercises = [],
-        exerView  = [[], [], [], [], [], []],
-        firstDate = DateModule.formatDbDate(datesWeek.firstDate),
-        lastDate  = DateModule.formatDbDate(datesWeek.lastDate);
+        datesWeek   = DateModule.getDatesWeek(),
+        exercises   = [],
+        exerView    = [{}, {}, {}, {}, {}, {}],
+        firstDate   = DateModule.formatDbDate(datesWeek.firstDate),
+        lastDate    = DateModule.formatDbDate(datesWeek.lastDate),
+        currentDate = new Date();
 
-    console.log(lastDate, firstDate);
 
     exercises = await Exercise.find('all', {
         select : [
             'exercise.id as id',
             'exercise.date as date',
             'exercise.time as time',
+            'exercise.number as number',
             'subject.title as subTitle',
+            'subject.id',
+            'user.firstname as teacherFirstName',
+            'user.lastname as teacherLastName',
+            'user.patronyc as teacherPatronyc',
+            'group.title as gtitle'
+        ],
+        where: [
+            ['user.id = ', req.session.userIndentity.id,' AND '],
+            ['date >= ', firstDate, 'AND'],
+            ['date >= ', firstDate, ''],
+        ],
+        join: [
+            ['inner', 'group', 'exercise.group_id = group.id'],
+            ['inner', 'subject', 'subject.id = exercise.subject_id'],
+            ['inner', 'user_has_subject', 'user_has_subject.subject_id = subject.id'],
+            ['inner', 'user', 'user_has_subject.user_id = user.id'],
+        ],
+    });
+    console.log(exercises);
+
+    for(let i = 0; i < exerView.length; i++){
+        if(currentDate.getDay() - 1 == i){
+            exerView[i].current = true;
+        }
+        exerView[i].exercises = [];
+        for(let j = 0; j < 5; j++){
+            exerView[i].exercises.push({
+                id      : 0,
+                time    : '',
+                subTitle: '',
+                teacher : '',
+                number  : '',
+                gtitle  : '',
+            });
+        }
+    }
+
+    for(let i = 0; i < exercises.length; i++){
+        let
+            date = new Date(exercises[i].date),
+            day  = date.getDay();
+
+        exerView[day - 1].exercises[exercises[i].number - 1].id       = exercises[i].id;
+        exerView[day - 1].exercises[exercises[i].number - 1].time     = exercises[i].time;
+        exerView[day - 1].exercises[exercises[i].number - 1].number   = exercises[i].number;
+        exerView[day - 1].exercises[exercises[i].number - 1].subTitle = exercises[i].subTitle;
+        exerView[day - 1].exercises[exercises[i].number - 1].gtitle   = exercises[i].gtitle;
+        exerView[day - 1].exercises[exercises[i].number - 1].teacher  = exercises[i].teacherLastName + exercises[i].teacherFirstName.substr(0, 1) + '. ' + exercises[i].teacherPatronyc.substr(0, 1) + '. ';
+    }
+
+    res.render('index/student', {
+        exerView : exerView,
+        exercises: exercises,
+    });
+}
+
+
+exports.actionIndexStudent = async (req, res) => {
+    let
+        datesWeek   = DateModule.getDatesWeek(),
+        exercises   = [],
+        exerView    = [{}, {}, {}, {}, {}, {}],
+        firstDate   = DateModule.formatDbDate(datesWeek.firstDate),
+        lastDate    = DateModule.formatDbDate(datesWeek.lastDate),
+        currentDate = new Date();
+
+
+    exercises = await Exercise.find('all', {
+        select : [
+            'exercise.id as id',
+            'exercise.date as date',
+            'exercise.time as time',
+            'exercise.number as number',
+            'subject.title as subTitle',
+            'subject.id',
             'user.firstname as teacherFirstName',
             'user.lastname as teacherLastName',
             'user.patronyc as teacherPatronyc',
@@ -43,19 +115,19 @@ exports.actionIndexStudent = async (req, res) => {
             ['date >= ', firstDate, ''],
         ],
         join: [
-            [
-                'inner', 'subject', 'subject.id = subject_id',
-                'inner', 'user_has_subject', 'user_has_subject.subject_id = subject.id',
-                'inner', 'user', 'user_has_subject.user_id = user.id'
-            ],
+            ['inner', 'subject', 'subject.id = subject_id'],
+            ['inner', 'user_has_subject', 'user_has_subject.subject_id = subject.id'],
+            ['inner', 'user', 'user_has_subject.user_id = user.id'],
         ],
     });
 
-    for(let i = 0; i < exerView.length; i++){
-        for(let j = 0; j < 5; j++){
-<<<<<<< HEAD
 
-=======
+    for(let i = 0; i < exerView.length; i++){
+        if(currentDate.getDay() - 1 == i){
+            exerView[i].current = true;
+        }
+        exerView[i].exercises = [];
+        for(let j = 0; j < 5; j++){
             exerView[i].exercises.push({
                 id      : 0,
                 time    : '',
@@ -63,21 +135,10 @@ exports.actionIndexStudent = async (req, res) => {
                 teacher : '',
                 number  : '',
             });
->>>>>>> 36e7f1d7df23c268ba815f732a8b98913a4303a9
         }
     }
 
     for(let i = 0; i < exercises.length; i++){
-<<<<<<< HEAD
-        let date = new Date(exercises[i].date);
-        switch(date.getDay()){
-            case 1:
-
-        }
-    }
-
-    res.send(exercises);
-=======
         let
             date = new Date(exercises[i].date),
             day  = date.getDay();
@@ -93,7 +154,6 @@ exports.actionIndexStudent = async (req, res) => {
         exerView : exerView,
         exercises: exercises,
     });
->>>>>>> 36e7f1d7df23c268ba815f732a8b98913a4303a9
 }
 
 exports.actionIndexAdmin = async(req, res) => {
