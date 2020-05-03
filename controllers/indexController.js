b7ce40fe1f5bb81be84b4c9c2eb885aafb164354
RobinLoksli <ami0504@mail.ru>
2020-05-03 14:52:03 +1000

    student index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -20,19 +20,20 @@ exports.actionIndexTeacher = async (req, res) => {
 
 exports.actionIndexStudent = async (req, res) => {
     let
-        datesWeek = DateModule.getDatesWeek(),
-        exercises = [],
-        exerView  = [[], [], [], [], [], []],
-        firstDate = DateModule.formatDbDate(datesWeek.firstDate),
-        lastDate  = DateModule.formatDbDate(datesWeek.lastDate);
+        datesWeek   = DateModule.getDatesWeek(),
+        exercises   = [],
+        exerView    = [{}, {}, {}, {}, {}, {}],
+        firstDate   = DateModule.formatDbDate(datesWeek.firstDate),
+        lastDate    = DateModule.formatDbDate(datesWeek.lastDate),
+        currentDate = new Date();
 
-    console.log(lastDate, firstDate);
 
     exercises = await Exercise.find('all', {
         select : [
             'exercise.id as id',
             'exercise.date as date',
             'exercise.time as time',
+            'exercise.number as number',
             'subject.title as subTitle',
             'subject.id',
             'user.firstname as teacherFirstName',
@@ -51,18 +52,30 @@ exports.actionIndexStudent = async (req, res) => {
     });
 
     for(let i = 0; i < exerView.length; i++){
+        if(currentDate.getDay() - 1 == i){
+            exerView[i].current = true;
+        }
+        exerView[i].exercises = [];
         for(let j = 0; j < 5; j++){
-            exerView[]
+            exerView[i].exercises.push({time: '', subTitle: '', teacher: '', number: ''});
         }
     }
 
     for(let i = 0; i < exercises.length; i++){
-        let date = new Date(exercises[i].date);
-        switch(date.getDay()){
-            case 1:
+        let
+            date = new Date(exercises[i].date),
+            day  = date.getDay();
 
-        }
+        // console.log(day, exercises[i].number);
+        // console.log(exerView[day - 1].exercises[exercises[i].number - 1]);
+
+        exerView[day - 1].exercises[exercises[i].number - 1].time     = exercises[i].time;
+        exerView[day - 1].exercises[exercises[i].number - 1].number   = exercises[i].number;
+        exerView[day - 1].exercises[exercises[i].number - 1].subTitle = exercises[i].subTitle;
+        exerView[day - 1].exercises[exercises[i].number - 1].time     = exercises[i].time;
     }
+
+    console.log(exerView[1]);
 
     res.send(exercises);
 }
