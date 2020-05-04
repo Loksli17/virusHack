f36090e1f3604fb889506d7db99f379dfccc12bb
BlackLirium <mrfilin97@gmail.com>
2020-05-04 15:44:58 +1000

    ControlModel

---
+++ models/ControlModel.js
@@ -0,0 +1,35 @@
+const modelMysql = require('../lib/Orm/mysqlOrm');
+
+//model of tasks-mysql
+function ControlModel(){
+    tableName = 'control';
+    modelMysql.call(this, tableName);
+}
+
+//наследование
+ControlModel.prototype = Object.create(modelMysql.prototype);
+ControlModel.prototype.constructor = ControlModel;
+
+ControlModel.prototype.rules = {
+    integer : ['pass', 'presence','user_id','exercise_id']
+
+};
+
+ControlModel.prototype.rulesMesseges = {
+    string  : 'Поле должно быть строкой',
+    integer : 'Поле должно быть числом',
+    date    : 'Поле должно иметь формат : DD-MM-YYYY',
+    required: 'Данное поле является обязательным',
+}
+
+ControlModel.prototype.fields = {
+    id       : 'ID',
+    presence : 'Присутствие',
+    pass     : 'Сдача',
+    user_id  : 'Id пользователя',
+    exercise_id :'Id занятия'
+}
+
+ControlModel.prototype.validateMessege = 'Ошибка отправки формы';
+
+module.exports = ControlModel;
