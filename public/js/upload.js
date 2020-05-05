90516876c0bcae75585ea3c5abb4db633d34e61a
Alexander <sansanichick@gmail.com>
2020-05-05 13:56:16 +1000

    fixes

--- public/js/upload.js
+++ public/js/upload.js
@@ -2,10 +2,12 @@ window.addEventListener("load", () => {
     let form = document.querySelector('.file-form');
     form.addEventListener('submit', upload, false);
 
-    let file = document.getElementById("file");
-    file.addEventListener("change", () => {
+    let fileField = document.getElementById("file");
+    console.log(fileField);
+    fileField.addEventListener("change", () => {
         let submit = document.getElementById("submit");
-        submit.style.display = "visible";
+        submit.style.display = "block";
+
     }, true);
 }, true);
 
@@ -23,13 +25,15 @@ function upload(e) {
     ajax.upload.onprogress = (e) => {
         let percent = e.loaded / e.total;
         console.log(e.loaded, e.total);
-        progress.style.display = "visible";
+        progress.style.display = "inline-block";
         progress.setAttribute('max', event.total);
         progress.value = event.loaded;
     }
 
     ajax.onload = ajax.onerror = function() {
         console.log(this.status);
+
+
 
         let newRow = document.createElement("div");
         newRow.className = "view-row";
@@ -42,13 +46,14 @@ function upload(e) {
         let fileDel = document.createElement("a");
         fileDel.className = "file-del";
         fileDel.href = "/file/delete?filename=" + file.name + "&path=/student";
-        fileDel.innerHTML = "Удалить";
-        let fa = document.createElement("i");
-        fa.className = "fa fa-spinner fa-spin";
+        // fileDel.innerHTML = "Удалить";
+        // let fa = document.createElement("i");
+        // fa.className = "fa fa-spinner fa-spin";
         viewContent.appendChild(fileDel);
-        viewContent.appendChild(fa);
+        // viewContent.appendChild(fa);
         newRow.appendChild(viewField);
         newRow.appendChild(viewContent);
+
 
         let filesField = document.getElementById("files-field");
         filesField.appendChild(newRow);
